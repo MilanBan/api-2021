58c076a84e04a26b89ac72d1cc618006f82b0b28
Milan Bancevic <m.bancevic@gmail.com>
2021-02-04 14:19:02 +0100

    revised to token authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,21 +28,26 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => ['required'],
-            'password' => ['required']
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
         ]);
-
-        if (Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(Auth::user(), 200);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
         }
-
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.']
-        ]);
+    
+        return $user->createToken($request->device_name)->plainTextToken;
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        Auth::logout();
+        $request->user()->tokens()->delete();
+
+        return response()->json('logout', 201);
     }
 }
